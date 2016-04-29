9e7ba69ff8bcf743aeb47e3ee2b2928ca4cab226
Igor Kim <yorikim@gmail.com>
2016-04-29 18:52:54 +0600

    add gem files

---
+++ lib/rack/http/signatures.rb
@@ -0,0 +1,9 @@
+require "rack/http/signatures/version"
+
+module Rack
+  module Http
+    module Signatures
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/http/signatures/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Http
+    module Signatures
+      VERSION = "0.1.0"
+    end
+  end
+end
