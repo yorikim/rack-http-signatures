a64c97cfa86a4317630d17e5a245b78fcdbcb834
Igor <yorikim@gmail.com>
2016-04-29 18:57:31 +0600

    Update signatures_spec.rb

--- spec/rack/http/signatures_spec.rb
+++ spec/rack/http/signatures_spec.rb
@@ -4,8 +4,4 @@ describe Rack::Http::Signatures do
   it 'has a version number' do
     expect(Rack::Http::Signatures::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
