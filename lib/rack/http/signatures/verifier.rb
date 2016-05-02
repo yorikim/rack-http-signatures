8b4544d3c818b00de3d4adbff80c3840017a342f
Igor Kim <yorikim@gmail.com>
2016-05-02 19:15:28 +0600

    fix travis ci issues

--- lib/rack/http/signatures/verifier.rb
+++ lib/rack/http/signatures/verifier.rb
@@ -3,6 +3,7 @@ require 'rack/http/signatures/cipher_factory'
 module Rack::Http::Signatures
   module Verifier
     def self.verify(algorithm, key, base64_signature, data)
+      return false unless key
       CipherFactory.create(algorithm).verify(key, Base64.decode64(base64_signature), data)
     end
   end
