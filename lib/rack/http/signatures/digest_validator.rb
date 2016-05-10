45c68025d31f8dad6e1cf0289ae071177852a31e
Igor Kim <yorikim@gmail.com>
2016-05-10 17:30:13 +0600

    fix thread-safe problem

--- lib/rack/http/signatures/digest_validator.rb
+++ lib/rack/http/signatures/digest_validator.rb
@@ -7,15 +7,13 @@ module Rack
     module Signatures
       class DigestValidator
         class << self
-          SHA256 = OpenSSL::Digest::SHA256.new
-
           def valid?(body, digest_header)
             digest = Digest.from_string(digest_header)
             return false unless digest.valid_algorithm?
 
-            SHA256.reset
-            SHA256 << body
-            SHA256.digest == digest.body
+            sha256 = OpenSSL::Digest::SHA256.new
+            sha256 << body
+            sha256.digest == digest.body
           end
         end
       end
